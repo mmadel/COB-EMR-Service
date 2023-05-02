ff014a8b794fcb12934169cf18053bc69774e512
mmadel <mohammed.adel82@gmail.com>
2023-05-02 16:25:32 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# COB-EMR-Service" 
---
+++ src/main/java/com/emr/ElectronicMedicalRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.emr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicMedicalRecordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicMedicalRecordsApplication.class, args);
+	}
+
+}
