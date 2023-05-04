e0040d7b728827a457ce5f3efc9a88bf9beed44b
mmadel <mohammed.adel82@gmail.com>
2023-05-04 19:54:26 +0300

    impl organization finder service

--- src/main/java/com/cob/emr/controller/OrganizationController.java
+++ src/main/java/com/cob/emr/controller/OrganizationController.java
@@ -3,6 +3,7 @@ package com.cob.emr.controller;
 import com.cob.emr.model.organization.OrganizationModel;
 import com.cob.emr.response.ResponseHandler;
 import com.cob.emr.service.organization.creator.OrganizationCreatorService;
+import com.cob.emr.service.organization.finder.OrganizationFinderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +15,8 @@ public class OrganizationController {
 
     @Autowired
     OrganizationCreatorService creatorService;
+    @Autowired
+    OrganizationFinderService finderService;
 
     @PostMapping("/create")
     public ResponseEntity<Object> create(@RequestBody OrganizationModel model) {
@@ -29,5 +32,21 @@ public class OrganizationController {
     public ResponseEntity<Object> delete(@PathVariable("id") Long id) {
         creatorService.delete(id);
         return ResponseHandler.generateResponse("Successfully deleted Organization!", HttpStatus.OK);
+    }
+
+    @GetMapping("/find")
+    public ResponseEntity<Object> findAll() {
+        return ResponseHandler
+                .generateResponse("Successfully deleted Organization!",
+                        HttpStatus.OK,
+                        finderService.findAll());
+    }
+
+    @GetMapping("/find/id/{id}")
+    public ResponseEntity<Object> findById(@PathVariable("id") Long id) {
+        return ResponseHandler
+                .generateResponse("Successfully deleted Organization!",
+                        HttpStatus.OK,
+                        finderService.findById(id));
     }
 }
--- src/main/java/com/cob/emr/service/organization/finder/OrganizationFinderServiceImpl.java
+++ src/main/java/com/cob/emr/service/organization/finder/OrganizationFinderServiceImpl.java
@@ -1,19 +1,39 @@
 package com.cob.emr.service.organization.finder;
 
 import com.cob.emr.model.organization.OrganizationModel;
+import com.cob.emr.repositories.organization.OrganizationRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class OrganizationFinderServiceImpl implements OrganizationFinderService {
+
+    @Autowired
+    OrganizationRepository repository;
+
+    @Autowired
+    ModelMapper mapper;
+
     @Override
     public List<OrganizationModel> findAll() {
-        return null;
+        List<OrganizationModel> result = new ArrayList<>();
+        repository.findAll()
+                .forEach(organization ->
+                        result.add(mapper.map(organization, OrganizationModel.class)));
+        return result;
     }
 
     @Override
     public OrganizationModel findById(Long id) {
-        return null;
+        return mapper
+                .map(repository.findById(id)
+                                .orElseThrow(() -> new IllegalArgumentException(id.toString()))
+                        , OrganizationModel.class);
+
+
     }
 }
