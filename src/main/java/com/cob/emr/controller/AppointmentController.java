dcd7ef5cea68c486cc6697c9109e8f2ed2b6cc4c
mmadel <mohammed.adel82@gmail.com>
2023-05-09 13:17:42 +0300

    impl create appointment operation

--- src/main/java/com/cob/emr/controller/AppointmentController.java
+++ src/main/java/com/cob/emr/controller/AppointmentController.java
@@ -2,6 +2,8 @@ package com.cob.emr.controller;
 
 import com.cob.emr.model.appointment.AppointmentModel;
 import com.cob.emr.response.ResponseHandler;
+import com.cob.emr.service.appointment.AppointmentCreatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/appointment")
 public class AppointmentController {
+
+    @Autowired
+    AppointmentCreatorService creatorService;
+
     @PostMapping("/create")
     public ResponseEntity<Object> create(@RequestBody AppointmentModel model) {
         return ResponseHandler
                 .generateResponse("Successfully added Appointment",
                         HttpStatus.OK,
-                        null);
+                        creatorService.create(model));
     }
 
     @PostMapping("/update")
--- src/main/java/com/cob/emr/service/appointment/AppointmentCreatorService.java
+++ src/main/java/com/cob/emr/service/appointment/AppointmentCreatorService.java
@@ -1,13 +1,49 @@
 package com.cob.emr.service.appointment;
 
+import com.cob.emr.entity.appointment.Appointment;
+import com.cob.emr.entity.appointment.AppointmentType;
+import com.cob.emr.entity.clinic.Clinic;
+import com.cob.emr.entity.patient.Patient;
+import com.cob.emr.entity.patient.PatientCase;
 import com.cob.emr.model.appointment.AppointmentModel;
+import com.cob.emr.repositories.appointment.AppointmentRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppointmentCreatorService {
 
+    @Autowired
+    AppointmentRepository appointmentRepository;
+    @Autowired
+    ModelMapper mapper;
+
     public AppointmentModel create(AppointmentModel model) {
+        Appointment toBeCreated = mapper.map(model, Appointment.class);
+        fillAppointmentAssociation(toBeCreated, model);
+        appointmentRepository.save(toBeCreated);
         return null;
+    }
+
+
+    public void fillAppointmentAssociation(Appointment toBeFilled, AppointmentModel model) {
+        Clinic clinic = new Clinic();
+        clinic.setId(model.getClinicId());
+
+        AppointmentType appointmentType = new AppointmentType();
+        appointmentType.setId(model.getAppointmentTypeId());
+
+        Patient patient = new Patient();
+        patient.setId(model.getPatientId());
+
+        PatientCase patientCase = new PatientCase();
+        patientCase.setId(model.getPatientCaseId());
+
+        toBeFilled.setClinic(clinic);
+        toBeFilled.setPatient(patient);
+        toBeFilled.setPatientCase(patientCase);
+        toBeFilled.setAppointmentType(appointmentType);
     }
 
 }
