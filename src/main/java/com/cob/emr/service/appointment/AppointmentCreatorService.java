ca96d1c1099165f854a4a0959f26a3bb9fbbce89
mmadel <mohammed.adel82@gmail.com>
2023-05-09 12:48:44 +0300

    create skeleton for create appointment service

--- src/main/java/com/cob/emr/service/appointment/AppointmentCreatorService.java
+++ src/main/java/com/cob/emr/service/appointment/AppointmentCreatorService.java
@@ -1,9 +1,13 @@
 package com.cob.emr.service.appointment;
 
+import com.cob.emr.model.appointment.AppointmentModel;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppointmentCreatorService {
 
+    public AppointmentModel create(AppointmentModel model) {
+        return null;
+    }
 
 }
