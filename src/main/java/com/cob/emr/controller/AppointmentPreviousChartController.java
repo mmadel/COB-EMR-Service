5c1609fd31985b1d8fefab6ce25771241ec714e9
mmadel <mohammed.adel82@gmail.com>
2023-05-19 18:42:05 +0300

    change findAllPreviousAppointments controller to fit with pagination

--- src/main/java/com/cob/emr/controller/AppointmentPreviousChartController.java
+++ src/main/java/com/cob/emr/controller/AppointmentPreviousChartController.java
@@ -8,10 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/appointment/chart/previous")
@@ -20,27 +17,27 @@ public class AppointmentPreviousChartController {
     @Autowired
     AppointmentChartPreviousFinderService finderService;
 
-    @GetMapping(path = "/find/patientId/{patientId}/clinicId/{clinicId}/page/{page}/size/{size}")
+    @GetMapping(path = "/find/patientId/{patientId}/clinicId/{clinicId}")
     public ResponseEntity<Object> findAllPreviousAppointments(@PathVariable Long patientId,
                                                               @PathVariable Long clinicId,
-                                                              @PathVariable() int page,
-                                                              @PathVariable() int size) {
-        Pageable paging = PageRequest.of(page, size, Sort.by("startDate").descending());
+                                                              @RequestParam(name = "offset") String offset,
+                                                              @RequestParam(name = "limit") String limit) {
+        Pageable page = PageRequest.of(Integer.parseInt(offset), Integer.parseInt(limit),Sort.by("startDate").descending());
         return ResponseHandler
-                .generateResponse("Successfully find All Previous Appointments",
-                        HttpStatus.OK,
-                        finderService.findAllPreviousAppointments(patientId, clinicId, paging));
+                .generateResponse("Successfully find All Incoming Appointments",
+                        HttpStatus.OK,null,
+                        finderService.findAllPreviousAppointments(patientId, clinicId, page));
     }
 
     @GetMapping(path = "/find/patientId/{patientId}/clinicId/{clinicId}/patientCaseId/{patientCaseId}/page/{page}/size/{size}")
     public ResponseEntity<Object> findPreviousAppointmentsByCase(@PathVariable Long patientId, @PathVariable Long clinicId,
                                                                  @PathVariable Long patientCaseId,
-                                                                 @PathVariable() int page,
-                                                                 @PathVariable() int size) {
-        Pageable paging = PageRequest.of(page, size, Sort.by("startDate").descending());
+                                                                 @RequestParam(name = "offset") String offset,
+                                                                 @RequestParam(name = "limit") String limit) {
+        Pageable page = PageRequest.of(Integer.parseInt(offset), Integer.parseInt(limit),Sort.by("startDate").descending());
         return ResponseHandler
                 .generateResponse("Successfully find Previous Appointments By Case",
                         HttpStatus.OK,
-                        finderService.findPreviousAppointmentsByCase(patientId, clinicId, patientCaseId, paging));
+                        finderService.findPreviousAppointmentsByCase(patientId, clinicId, patientCaseId, page));
     }
 }
