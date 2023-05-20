bde544ec60a8d8f70299cac736b918fc8bd6e260
mmadel <mohammed.adel82@gmail.com>
2023-05-20 13:21:44 +0300

    paginate findCancelNoShowAppointments

--- src/main/java/com/cob/emr/controller/AppointmentCancelNoShowChartController.java
+++ src/main/java/com/cob/emr/controller/AppointmentCancelNoShowChartController.java
@@ -8,10 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/appointment/chart/cno")
@@ -44,15 +41,15 @@ public class AppointmentCancelNoShowChartController {
                         finderService.findNoShowAppointments(patientId, clinicId, clinicId, paging));
     }
 
-    @GetMapping(path = "/find/cancel/noshow/patientId/{patientId}/clinicId/{clinicId}/patientCaseId/{patientCaseId}/page/{page}/size/{size}")
+    @GetMapping(path = "/find/cancel/noshow/patientId/{patientId}/clinicId/{clinicId}/patientCaseId/{patientCaseId}")
     public ResponseEntity<Object> findCancelNoShowAppointments(@PathVariable Long patientId, @PathVariable Long clinicId,
                                                                @PathVariable Long patientCaseId,
-                                                               @PathVariable() int page,
-                                                               @PathVariable() int size) {
-        Pageable paging = PageRequest.of(page, size, Sort.by("startDate").descending());
+                                                               @RequestParam(name = "offset") String offset,
+                                                               @RequestParam(name = "limit") String limit) {
+        Pageable page = PageRequest.of(Integer.parseInt(offset), Integer.parseInt(limit),Sort.by("startDate").descending());
         return ResponseHandler
                 .generateResponse("Successfully find Cancel No-Show Appointments",
                         HttpStatus.OK,
-                        finderService.findCancelNoShowAppointments(patientId, clinicId, clinicId, paging));
+                        finderService.findCancelNoShowAppointments(patientId, clinicId, clinicId, page));
     }
 }
